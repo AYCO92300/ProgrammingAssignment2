db932a10dac39b6099af029f238d609555ae2d2e
AYCO92300 <y.cohenostrowiak@gmail.com>
2017-12-18 11:40:31 +0100

    hand-in Assignement module 2 week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Dear grader, please find below R functions to create a cache functionality to inversed
+## matrices:
 
-## Write a short comment describing this function
+## 1- Function n1 is makeCacheMatrix --> provides a storage for a defined square matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+		matrixinv <- NULL 	## this is to create an empty matrix that will be used      						    
+		                        ## to store inversed matrix when setup in an R object
+		
+		set <- function(y){    ## Helps to create and set the matrix as computed in the							 
+		                       ## function above to be stored in the list
+			x <<- y
+			matrixinv <<- NULL
+		}
+		get <- function()x      ## Call the function to retrieve the matrix
+		setinverse <- function(inv) matrixinv <<- inv
+		getinverse <- function() matrixinv
+		invisible(list(set=set,get=get, setinverse= setinverse, getinverse=getinverse)) 					
+		                        ## create a list to be used in cacheSolve below to print 							    
+		                        ## the inversed matrix [INVISIBLE is to avoid misleading the 							
+		                        ## end user of the fonction when using the prompt]
+		
 }
 
-
-## Write a short comment describing this function
-
+## 2- Function n2 is cacheSolve --> allows to return the inverse of the matrix previously stored into makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        matrixinv = x$getinverse() ## Assign the  matrix  previsously stored in getinverse to the empty matrix variable
+        if(!is.null(matrixinv)){
+				return(matrixinv)
+}
+        InitMatrix = x$get()
+        matrixinv = solve(InitMatrix, ...) 
+        
+        # sets the value of the inverse in the cache via the setinv function.
+        x$setinv(matrixinv)
+        
+        return(matrixinv)
 }
